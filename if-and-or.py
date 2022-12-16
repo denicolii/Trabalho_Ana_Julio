23bb5f7f5f48081da00cc7c17ce22d30c8174c99
denicolii <110945232+denicolii@users.noreply.github.com>
2022-12-16 18:47:35 -0300

    Add files via upload

---
+++ if-and-or.py
@@ -0,0 +1,11 @@
+# alunos: Ana Denicoli e Julio Cesar
+#função: programa que diz se o usuario está apto para trabalhar como jovem aprendiz.
+
+idade = int(input('Digite sua idade: '))
+
+if idade >= 14  and idade <= 24 :
+    print ('Você ESTÁ apto para trabalhar como jovem aprendiz.')
+
+else :
+    print ('Você NÃO está entre as idades permitidas para trabalhar como jovem aprendiz.')
+
