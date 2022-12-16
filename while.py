30e38f9867fb14856333116f340a0eeb21ae375c
denicolii <110945232+denicolii@users.noreply.github.com>
2022-12-16 17:36:23 -0300

    Add files via upload

---
+++ while.py
@@ -0,0 +1,11 @@
+# Alunos : Ana denicoli e  Julio Cesar
+# turma : T2
+# função : Programa que pede para o usuario digitar o nome de 5 países
+
+pais = 0
+
+while pais <= 4 :
+    input('Digite o nome de um país: ')
+    pais = pais+1
+
+
