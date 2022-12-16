ff923f773a1f5880c81816ba91ef111d52aa397b
denicolii <110945232+denicolii@users.noreply.github.com>
2022-12-16 17:20:58 -0300

    Add files via upload

---
+++ aleatorio.py
@@ -0,0 +1,13 @@
+# Alunos : Ana denicoli e  Julio Cesar
+# turma : T2
+# função : Programa que gera 10 valores aleatórios para o usuario jogar na Mega Sena
+
+import random
+
+for x in range (0,10) :
+    valor = random.randint(0,100)
+    print ('Os valores para a Mega Sena são: ', valor)
+
+
+
+
